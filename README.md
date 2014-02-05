b162c3c40ce4590a653ca27e1eb1a45c5a95808b
Christian Vozar <christian@rogueethic.com>
2014-02-05 15:28:07 -0600

    Working version

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
-itunes-hipchat-status
-=====================
+# Apple iTunes to Atlassian HipChat Status
 
-Update Atlassian HipChat User Status to Current iTunes Track &amp; Artist
+A simple application that takes the current track and artist from iTunes and updates your Atlassian HipChat status to reflect.
+
+## Requirements
+
+- While the application is written in Google Go, it will spawn a process that utilizes AppleScript to check iTunes for the current track. So any OSX should do but this is tested against the latest release, 10.9.
+- Compilation requires Google Go 1.2
+- Atlassian HipChat account
+
+## Installation
+
+The application uses only the standard Go libraries so installation is as simple as compiling the binary on your platform; assuming your GOROOT and GOPATH are setup correctly.
+
+```
+$ git clone https://github.com/christianvozar/itunes-hipchat-status.git
+$ go build
+```
+
+## Usage
+
+From the commandline you can utilize the application by passing in your HipChat username and [authentication token](https://www.hipchat.com/docs/apiv2/auth).
+```
+./itunes-hipchat-status -user=yourname@domain.com -token=XXXXXXXXXXXXXXXXXXXXXXXXXX
+```
+
+A more appropriate usage is to schedule the updater to run on a cron schedule of every 15 or 30 seconds.
+
+
+## License
+
+Copyright 2014, Rogue Ethic, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ itunes-hipchat-status.go
@@ -0,0 +1,147 @@
+// Copyright 2014, Rogue Ethic, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Contributor(s):
+//   Christian Vozar (christian@rogueethic.com)
+
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+const (
+	HipChatApiUrl     = "https://api.hipchat.com"
+	HipChatApiVersion = "v2"
+)
+
+type HipChatUser struct {
+	Name         string       `json:"name,omitempty"`
+	Title        string       `json:"title,omitempty"`
+	Presence     presenceInfo `json:"presence,omitempty"`
+	MentionName  string       `json:"mention_name,omitempty"`
+	Timezone     string       `json:"timezone,omitempty"`
+	Email        string       `json:"email,omitempty"`
+	IsGroupAdmin bool         `json:"is_group_admin"`
+}
+
+type presenceInfo struct {
+	Status string `json:"status, show"`
+	Show   string `json:"show, show"`
+}
+
+func main() {
+	flagVersion := flag.Bool("version", false, "Display application version.")
+	flagUser := flag.String("user", "", "Atlassian HipChat ID or Email of user to update.")
+	flagAuthToken := flag.String("token", "", "Atlassian HipChat API v2 authentication token.")
+	flag.Parse()
+
+	// Handle no command-line paramters
+	if flag.NFlag() == 0 {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	// Output version
+	if *flagVersion {
+		fmt.Println("iTunes to Atlassian HipChat Status", Version)
+		os.Exit(0)
+	}
+
+	userInformation := viewHipChatUser(*flagUser, *flagAuthToken)
+	userInformation.Presence.Status = getItunesInformation()
+	updateHipChatUser(userInformation, *flagUser, *flagAuthToken)
+}
+
+func updateHipChatUser(u HipChatUser, e, a string) {
+	messageUri := fmt.Sprintf("%s/%s/user/%s?auth_token=%s", HipChatApiUrl, HipChatApiVersion, e, a)
+
+	messagePayload, err := json.Marshal(u)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body := strings.NewReader(string(messagePayload))
+
+	httpClient := &http.Client{}
+	req, err := http.NewRequest("PUT", messageUri, body)
+	req.Header.Add("content-type", "application/json")
+
+	_, err = httpClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func viewHipChatUser(e, a string) HipChatUser {
+	var hipChatData HipChatUser
+
+	messageUri := fmt.Sprintf("%s/%s/user/%s?auth_token=%s", HipChatApiUrl, HipChatApiVersion, e, a)
+	res, err := http.Get(messageUri)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(body, &hipChatData)
+
+	return hipChatData
+}
+
+func getItunesInformation() string {
+	appleScriptRuntime := "osascript"
+	arg0 := "-e"
+	cmd := exec.Command(appleScriptRuntime, arg0, `tell application "iTunes"
+set trackname to name of current track
+set artistname to artist of current track
+set albumname to album of current track
+
+if artistname is null then
+set artistshow to ""
+else if artistname is "" then
+set artistshow to ""
+else
+set artistshow to " | " & artistname & ""
+end if
+
+set output to trackname & artistshow
+end tell`)
+
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	iTunesInformation := strings.TrimSpace(string(out))
+
+	// HipChat status cannot exceed 50 characters.
+	if len(iTunesInformation) > 50 {
+		return iTunesInformation[0:46] + "..."
+	}
+
+	return iTunesInformation
+}
